bc805550921683bba66b31a5db9d838d37ffc65d
Haoran-Bao <albert_bao@hotmail.com>
2026-01-19 01:29:02 +0100

    debug testing scripts

---
+++ scripts/FashionMNIST/ours_flexible_debug.sh
@@ -0,0 +1,53 @@
+cmdargs=$1
+
+# `gpu=$1`
+# `echo "export CUDA_VISIBLE_DEVICES=${gpu}"`
+#export CUDA_VISIBLE_DEVICES='0,1'
+#"FedAVG","median", "NormBound","trmean","krum","flame"
+export CUDA_VISIBLE_DEVICES='1'
+hyperparameters04='[{
+    "random_seed" : [4],
+
+    "dataset" : ["fmnist"],
+    "models" : [{"ConvNet" : 10}],
+
+    "attack_methods" : [{"Fang" : 2, "label_flip" : 1}],
+    "participation_rate" : [1],
+
+    "alpha" : [0.1],
+
+    "distill_interval": [1],
+    "communication_rounds" : [20],
+    "local_epochs" : [1],
+    "batch_size" : [32],
+
+    "local_optimizer" : [ ["SGD", {"lr": 0.001}]],
+
+    "aggregation_mode" : ["REDefense_fedavg"],
+
+    "sample_size": [0],
+    "syn_steps" : [2],
+    "lr_img": [0.5],
+    "lr_teacher": [0.1],
+    "lr_label": [0.2],
+    "lr_lr": [5e-6],
+    "img_optim": ["sgd"],
+    "lr_optim": ["sgd"],
+    "save_scores" : [false],
+    "Iteration" : [200],
+    "Max_Iter" : [400],
+
+    "interval": [20],
+    "pretrained" : [null],
+    "save_model" : [null],
+    "log_frequency" : [1],
+    "log_path" : ["new_noniid/"]}]
+
+'
+
+
+RESULTS_PATH="results/"
+DATA_PATH="../data/"
+CHECKPOINT_PATH="checkpoints/"
+
+python -u codes/run_ours.py --hp="$hyperparameters04" --RESULTS_PATH="$RESULTS_PATH" --DATA_PATH="$DATA_PATH" --CHECKPOINT_PATH="$CHECKPOINT_PATH" $cmdargs 
